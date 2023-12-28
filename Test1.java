736b8f92122d7eb0fa89a61b85d93a6970992973
Reantaro <miterel1441@gmail.com>
2023-12-29 02:09:23 +0300

    start

---
+++ Test1.java
@@ -0,0 +1,9 @@
+public class Test1 {
+    //дальше идет только заготовка, ничего более.
+if(voic == true) {
+        recordOn;
+    }else(voic == false) {
+        recordOff}
+
+//Бред выше задает простейшую проверку команды.
+}
